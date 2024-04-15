1d04c9dfbfefae9e37c851cf808a104b0bfa04ef
Ophir-Maor <166450286+Ophir-Maor@users.noreply.github.com>
2024-04-14 23:34:44 -0700

    Update ItemPage.java

--- code/src/cisc191/app/ItemPage.java
+++ code/src/cisc191/app/ItemPage.java
@@ -2,5 +2,6 @@ package cisc191.app;
 
 public class ItemPage
 {
-
+	// This page will hold the pictures of products, as well as options to search
+	JLabel WelcomeLabel = new JLabel("Welcome to our online store! Click on an item, or search to continue");
 }
